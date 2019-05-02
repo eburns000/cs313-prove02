0b98883cb6dac8d335ceb604de1a90d5812921c8
Eric Burns <eburns000@byui.edu>
2019-05-01 20:41:47 -0700

    Update assignments, index, navbar, style

--- web/navbar.php
+++ web/navbar.php
@@ -4,9 +4,17 @@
  * File: navbar.php
  * Defines a standard navigation bar for each page
  *******************************************************/
-echo "Test Include Navbar";
-echo "Test 2";
-
-
-
+echo '
+      <div class="btn-group" id="navbar">
+        <button type="button" class="btn btn-primary">
+          <a class="align-middle customLink" href="../index.html">Home</a>
+        </button>
+        <button type="button" class="btn btn-primary">
+          <a class="align-middle customLink" href="../assignments.html">Assignments</a>
+        </button>
+        <button type="button" class="btn btn-primary">Link 2</button>
+        <button type="button" class="btn btn-primary">Link 3</button>
+        <button type="button" class="btn btn-primary">Link 4</button>      
+      </div>
+';
 ?>
