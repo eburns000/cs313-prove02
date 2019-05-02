e4c2bdc0a4cf39948dda51ce1a6a6e38b66c7266
Eric Burns <eburns000@byui.edu>
2019-05-01 18:32:08 -0700

    first push

---
+++ web/navbar.php
@@ -0,0 +1,12 @@
+<?php
+
+/*******************************************************
+ * File: navbar.php
+ * Defines a standard navigation bar for each page
+ *******************************************************/
+echo "Test Include Navbar";
+echo "Test 2";
+
+
+
+?>
