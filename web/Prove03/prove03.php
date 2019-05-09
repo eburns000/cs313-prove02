74ce3ce492fb405edde97a8a28c0730df05c98ce
Eric Burns <eburns000@byui.edu>
2019-05-09 13:00:24 -0700

    update

--- web/Prove03/prove03.php
+++ web/Prove03/prove03.php
@@ -62,7 +62,7 @@
     <!-- Navigation -->
     <div class="container">
       <nav class="navbar navbar-expand-md bg-dark fixed-top">
-        <h2>Potato Emporium</h2>
+        <h2>Potato Emporium</h2><br>
         <h5>Premium, Organic Potatoes Delivered Fresh to Your Door</h5> 
       </nav>
     </div>
