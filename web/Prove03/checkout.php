be2b041ea3caa385f383113a5067430c3ad5aff1
Eric Burns <eburns000@byui.edu>
2019-05-09 07:24:22 -0700

    add checkout and confirm pages

---
+++ web/Prove03/checkout.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prove 03 - Eric Burns</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+
+	<form action="confirm.php" method="post" class="form-checkout">
+
+	<div class="container-fluid main">
+
+		<div class="row row-checkout">
+
+			Address 1: <input class="field-checkout" type="text" name="address1"><br>
+			Address 2: <input class="field-checkout" type="text" name="address2"><br>
+			City: <input class="field-checkout" type="text" name="city"><br>
+			State: <input class="field-checkout" type="text" name="state"><br>
+			Zip: <input class="field-checkout" type="text" name="zip"><br>
+
+		</div>
+
+		<div class="row row-checkout">
+			<input class="complete-purchase" type="submit" value="Complete Purchase">
+		</div>
+
+	</div>
+
+	</form>
+
+	<form action="cart.php" method="post" class="form-submit">
+        <input class="view-cart" type="submit" value="Return to Cart">
+    </form>	
+
+</body>
+</html>
+
---
+++ web/Prove03/confirm.php
@@ -0,0 +1,120 @@
+<?php
+
+	session_start();
+
+	$address1 = $_POST["address1"];
+	$address2 = $_POST["address2"];
+	$city = $_POST["city"];
+	$state = $_POST["state"];
+	$zip = $_POST["zip"];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prove 03 - Eric Burns</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+
+	<div class="container-fluid main">
+
+		<h4>Purchase Confirmation</h4>	
+		<p>
+			Thank you for your purchase today from Potatoes Emporium. Your complete satisfaction is our Number 1 concern. If you are not satisfied for any reason, you any unused potatoes for a full refund.
+		</p>
+		<br>
+
+		<h6>Order Summary</h6>
+
+			<table class="order-confirm-table">
+
+				<tr class="order-confirm-table-header">
+					<th>Item</th>
+					<th>Quantity (lbs)</th>
+					<th>Price/lb</th>
+					<th>Price</th>
+				</tr>
+
+				<?php
+
+				$length = intval($_SESSION["numItems"]);
+
+				$totalCost = 0;
+
+				for ($i = 0; $i < $length; $i++) {
+
+					if(array_key_exists("cart" . $i, $_SESSION) && $_SESSION["cart" . $i][1] > 0) {
+
+						$totalCost += $_SESSION["cart" . $i][3];
+
+						echo '
+						<tr class="order-confirm-table-row">
+							<td>';
+
+						echo $_SESSION["cart" . $i][0];
+
+						echo '</td>
+							<td>';
+
+						echo $_SESSION["cart" . $i][1];	
+
+						echo '</td>
+							<td>';
+
+						echo $_SESSION["cart" . $i][2];
+
+
+						echo '</td>
+							<td>';
+
+						echo $_SESSION["cart" . $i][3];
+
+						echo '</td>							
+						</tr>';
+
+					}
+
+				}
+
+				?>
+
+				<tr class="order-confirm-table-row">
+					<td>Total</td>
+					<td></td>
+					<td></td>
+					<td><?php echo '$' . $totalCost; ?></td>
+				</tr>
+
+			</table>
+
+		<h6>Shipping Information</h6>
+		<p>
+			Your items will be shipped within 24 hours to the address below. If the address is incorrect, please contact customer service immediatly so your shipment can be re-routed.
+		</p>
+		<br>
+
+		<!-- Address Summary -->
+		<?php
+			echo 'Address: ' . $address1 . (!emtpy($address2) ? ', ' . $address2 : '') . '<br>';
+			echo 'City: ' . $city . '<br>';
+			echo 'State: ' . $state . '<br>';
+			echo 'Zip: ' . $zip . '<br';
+		?>
+
+		<h6>Thank you for shopping at Potatoes Emporium</h6>
+
+		<!-- Include here a button to return to home page and if so, reset the session information -->
+
+	</div>
+
+</body>
+</html>
