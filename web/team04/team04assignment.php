31fd8ff60a599f3e93bea9715dd4d524b8494e9c
Eric Burns <eburns000@byui.edu>
2019-05-20 09:40:11 -0700

    add team 04 assignment

---
+++ web/team04/team04assignment.php
@@ -0,0 +1,57 @@
+<?php 
+
+	try
+	{
+	  $dbUrl = getenv('DATABASE_URL');
+
+	  $dbOpts = parse_url($dbUrl);
+
+	  $dbHost = $dbOpts["host"];
+	  $dbPort = $dbOpts["port"];
+	  $dbUser = $dbOpts["user"];
+	  $dbPassword = $dbOpts["pass"];
+	  $dbName = ltrim($dbOpts["path"],'/');
+
+	  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $ex)
+	{
+	  echo 'Error!: ' . $ex->getMessage();
+	  die();
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Team 04 Assignment</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<!-- Results from Database Test -->
+	<h1>Scripture Resources</h1>
+	<br>
+	<?php 
+
+		foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+		{
+		  echo '<em>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</em>' . ' - ' . $row['content'];
+		  echo '<br/>';
+		}
+
+	?>
+	<br>
+
+</body>
+</html>
