2da28e998a2c547385e6476d885a2a76f0349038
Eric Burns <eburns000@byui.edu>
2019-05-20 09:07:31 -0700

    add team 04 folder

--- db/htpdb01.sql
+++ db/htpdb01.sql
@@ -6,79 +6,71 @@
 CREATE DATABASE htpdb01;
 
 CREATE TABLE clinic (
-	id					SERIAL			PRIMARY KEY,
-	active					BOOLEAN			NOT NULL DEFAULT TRUE,
-	clinic_name				VARCHAR(80)		NOT NULL UNIQUE
+	id					 SERIAL			 PRIMARY KEY,
+	active					 BOOLEAN			 NOT NULL DEFAULT TRUE,
+	clinic_name				 VARCHAR(80)		 NOT NULL UNIQUE
 	);
 
 CREATE TABLE account_type (
-	id					SERIAL			PRIMARY KEY,
-	active					BOOLEAN			NOT NULL DEFAULT TRUE,
-	account_type_name			VARCHAR(80)		NOT NULL UNIQUE
+	id					 SERIAL			 PRIMARY KEY,
+	active					 BOOLEAN	 		NOT NULL DEFAULT TRUE,
+	account_type_name			 VARCHAR(80) 		NOT NULL UNIQUE
 	);
 
 CREATE TABLE discipline (
-	id					SERIAL			PRIMARY KEY,
-	active					BOOLEAN			NOT NULL DEFAULT TRUE,
-	discipline_name				VARCHAR(80)		NOT NULL UNIQUE
+	id					 SERIAL	 		PRIMARY KEY,
+	active					 BOOLEAN	 		NOT NULL DEFAULT TRUE,
+	discipline_name				 VARCHAR(80)	 	NOT NULL UNIQUE
 	);
 
 CREATE TABLE modality (
-	id					SERIAL			PRIMARY KEY,
-	active					BOOLEAN			NOT NULL DEFAULT TRUE,
-	modality_name				VARCHAR(80)		NOT NULL UNIQUE
+	id					 SERIAL		 	PRIMARY KEY,
+	active					 BOOLEAN	 		NOT NULL DEFAULT TRUE,
+	modality_name				 VARCHAR(80) 		NOT NULL UNIQUE
 	);
 
 CREATE TABLE account (
-	id					SERIAL			PRIMARY KEY,
-	assigned_clinic_id			INT,
-	account_type_id				INT,
-	assigned_therapist_id			INT,
-	username				VARCHAR(80)		NOT NULL UNIQUE,
-	password				VARCHAR(80)		NOT NULL,
-	email					VARCHAR(80)		NOT NULL UNIQUE,
-	first_name				VARCHAR(80),
-	last_name				VARCHAR(80),
-	phone					VARCHAR(80),
-	my_points				INT			NOT NULL DEFAULT 0 CHECK (my_points >= 0), -- can't be negative
-	active					BOOLEAN			NOT NULL DEFAULT TRUE,
-	new_account				BOOLEAN			NOT NULL DEFAULT TRUE,	
-	locked					BOOLEAN			NOT NULL DEFAULT FALSE,
-	created_on				TIMESTAMP		WITH TIME ZONE NOT NULL DEFAULT NOW(),	
-	last_login				TIMESTAMP		WITH TIME ZONE,
-	FOREIGN KEY (assigned_clinic_id) 	REFERENCES clinic (id) 			ON DELETE RESTRICT ON UPDATE CASCADE,
-	FOREIGN KEY (account_type_id) 		REFERENCES account_type (id) 		ON DELETE RESTRICT ON UPDATE CASCADE,
-	FOREIGN KEY (assigned_therapist_id) 	REFERENCES account (id) 		ON DELETE RESTRICT ON UPDATE CASCADE
+	id					 SERIAL	 		PRIMARY KEY,
+	assigned_clinic_id			 INT,
+	account_type_id				 INT,
+	assigned_therapist_id		 	INT,
+	username				 VARCHAR(80)	 	NOT NULL UNIQUE,
+	password				 VARCHAR(80)	 	NOT NULL,
+	email					 VARCHAR(80)	 	NOT NULL UNIQUE,
+	first_name				 VARCHAR(80),
+	last_name				 VARCHAR(80),
+	phone					 VARCHAR(80),
+	my_points				 INT			 NOT NULL DEFAULT 0 CHECK (my_points >= 0), -- can't be negative
+	active					 BOOLEAN		 	NOT NULL DEFAULT TRUE,
+	new_account				 BOOLEAN		 	NOT NULL DEFAULT TRUE,	
+	locked					 BOOLEAN		 	NOT NULL DEFAULT FALSE,
+	created_on				 TIMESTAMP	 	WITH TIME ZONE NOT NULL DEFAULT NOW(),	
+	last_login				 TIMESTAMP	 	WITH TIME ZONE,
+	FOREIGN KEY (assigned_clinic_id) 	 REFERENCES clinic (id) 			 ON DELETE RESTRICT ON UPDATE CASCADE,
+	FOREIGN KEY (account_type_id) 		 REFERENCES account_type (id) 		 ON DELETE RESTRICT ON UPDATE CASCADE,
+	FOREIGN KEY (assigned_therapist_id)  	REFERENCES account (id) 		 ON DELETE RESTRICT ON UPDATE CASCADE
 	);
 
 CREATE TABLE exercise (
-	id					SERIAL			PRIMARY KEY,
-	discipline_id				INT			NOT NULL,
-	modality_id				INT			NOT NULL,
-	active					BOOLEAN			NOT NULL DEFAULT TRUE,
-	exercise_name				VARCHAR(80)		NOT NULL,
-	assignment				TEXT,
-	video_link				VARCHAR(255),
-	FOREIGN KEY (discipline_id) 		REFERENCES discipline (id) 		ON DELETE RESTRICT ON UPDATE CASCADE,
-	FOREIGN KEY (modality_id) 		REFERENCES modality (id) 		ON DELETE RESTRICT ON UPDATE CASCADE
+	id					 SERIAL		 	PRIMARY KEY,
+	discipline_id		 		INT	 		NOT NULL,
+	modality_id			 	INT		 	NOT NULL,
+	active				 	BOOLEAN	 		NOT NULL DEFAULT TRUE,
+	exercise_name		 		VARCHAR(80) 		NOT NULL,
+	assignment			 	TEXT,
+	video_link			 	VARCHAR(255),
+	FOREIGN KEY (discipline_id) 		 REFERENCES discipline (id) 		 ON DELETE RESTRICT ON UPDATE CASCADE,
+	FOREIGN KEY (modality_id) 		 REFERENCES modality (id) 		 ON DELETE RESTRICT ON UPDATE CASCADE
 	);
 
-CREATE TABLE assigned_exercises (
-	account_id				INT			NOT NULL,
-	exercise_id				INT			NOT NULL,
-	assigned_date				DATE			NOT NULL DEFAULT CURRENT_DATE,
-	active					BOOLEAN			NOT NULL DEFAULT TRUE,
-	point_value				INT			NOT NULL DEFAULT 0 CHECK (point_value >= 0),
-	completed				BOOLEAN			NOT NULL DEFAULT FALSE,
+CREATE TABLE assigned_exercise (
+	account_id			 	INT	 		NOT NULL,
+	exercise_id			 	INT	 		NOT NULL,
+	assigned_date			 	DATE 			NOT NULL DEFAULT CURRENT_DATE,
+	active					 BOOLEAN 			NOT NULL DEFAULT TRUE,
+	point_value				 INT	 		NOT NULL DEFAULT 0 CHECK (point_value >= 0),
+	completed				 BOOLEAN 			NOT NULL DEFAULT FALSE,
 	PRIMARY KEY (account_id, exercise_id),	-- each exercise id can only be assigned to a client once
-	FOREIGN KEY (account_id) 		REFERENCES account (id) 		ON DELETE RESTRICT ON UPDATE CASCADE,
-	FOREIGN KEY (exercise_id) 		REFERENCES exercise (id)		ON DELETE RESTRICT ON UPDATE CASCADE
+	FOREIGN KEY (account_id) 	 	REFERENCES account (id) 	 	ON DELETE RESTRICT ON UPDATE CASCADE,
+	FOREIGN KEY (exercise_id) 	 	REFERENCES exercise (id)	 	ON DELETE RESTRICT ON UPDATE CASCADE
 	);
-
-
-
-
-
-
-	
-
---
+++ web/team04/team04.php
@@ -0,0 +1,78 @@
+<?php 
+
+	try
+	{
+	  $dbUrl = getenv('DATABASE_URL');
+
+	  $dbOpts = parse_url($dbUrl);
+
+	  $dbHost = $dbOpts["host"];
+	  $dbPort = $dbOpts["port"];
+	  $dbUser = $dbOpts["user"];
+	  $dbPassword = $dbOpts["pass"];
+	  $dbName = ltrim($dbOpts["path"],'/');
+
+	  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $ex)
+	{
+	  echo 'Error!: ' . $ex->getMessage();
+	  die();
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Team 04 Exercise</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<!-- Results from Database Test -->
+	<h4>Clinic Table Data</h4>
+	<br>
+	<?php 
+
+		foreach ($db->query('SELECT clinic_name FROM clinic') as $row)
+		{
+		  echo 'Clinic Name: ' . $row['clinic_name'];
+		  echo '<br/>';
+		}
+
+	?>
+
+	<!-- Clinic Data Entry -->	
+	<h4>Update Static Tables</h4>
+	<br>
+	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="form-checkout">
+
+		<label for="clinic_name">Clinic Name</label><br>
+		<input class="field-checkout" type="text" placeholder="Clinic Name" name="clinic_name"><br>
+
+		<label for="account_type">Account Type</label><br>		
+		<input class="field-checkout" type="text" placeholder="Account Type" name="account_type"><br>
+
+		<label for="discipline">Discipline Name</label><br>		
+		<input class="field-checkout" type="text" placeholder="Discipline" name="discipline"><br>
+		
+		<label for="modality">Modality Name</label><br>
+		<input class="field-checkout" type="text" placeholder="Modality" name="modality"><br>
+
+		<input class="add-button" type="submit" value="Update Data">
+
+	</form>
+	<br>
+
+</body>
+</html>
