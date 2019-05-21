e99aabcb26c5c42a2ac4eaabcabf333fccfd4f4e
Eric Burns <eburns000@byui.edu>
2019-05-20 17:00:52 -0700

    update login.php

--- web/htp/login.php
+++ web/htp/login.php
@@ -27,22 +27,22 @@
 
   $message="";
 
-  if(!empty($_POST["login"])) {
-    $statement = $db->query('SELECT id, username, password FROM account WHERE username="' . $_POST['username'] . '" AND password="' . $_POST['password'] . '"');
-    $row = $statement->fetch(PDO::FETCH_ASSOC);
-    if(is_array($row)) {
-      $_SESSION["user_id"] = $row["id"];
-      $message = 'Login Successful';
-    } else {
-      $message = "Invalid Username or Password!";
-    }
-  }
+  // if(!empty($_POST["login"])) {
+  //   $statement = $db->query('SELECT id, username, password FROM account WHERE username="' . $_POST['username'] . '" AND password="' . $_POST['password'] . '"');
+  //   $row = $statement->fetch(PDO::FETCH_ASSOC);
+  //   if(is_array($row)) {
+  //     $_SESSION["user_id"] = $row["id"];
+  //     $message = 'Login Successful';
+  //   } else {
+  //     $message = "Invalid Username or Password!";
+  //   }
+  // }
 
-  if(!empty($_POST["logout"])) {
-    $_SESSION["user_id"] = "";
-    session_destroy();
-    $message = 'Logout Successful';
-  }
+  // if(!empty($_POST["logout"])) {
+  //   $_SESSION["user_id"] = "";
+  //   session_destroy();
+  //   $message = 'Logout Successful';
+  // }
 
 ?>
 
