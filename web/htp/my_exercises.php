1a615e04b1871d8c8b09ded31008fac46d8f0c32
Eric Burns <eburns000@byui.edu>
2019-05-25 12:31:41 -0700

    update

--- web/htp/my_exercises.php
+++ web/htp/my_exercises.php
@@ -2,7 +2,7 @@
 
   $login_user_id = $_SESSION['user_id'];
 
-  echo '<!-- Client Dashboard: Show list of Clients Exercises -->';
+  echo '<!-- Client Dashboard: Show list of Clients Assigned Exercises -->';
   echo '<h2>My Exercises</h2>';
   echo '<br>';
 
@@ -11,30 +11,65 @@
   echo '<th>First Name</th>';
   echo '<th>Last Name</th>';
   echo '<th>Email</th>';
+  echo '<th>Email</th>';
+  echo '<th>Email</th>';
+  echo '<th>Email</th>';
+  echo '<th>Email</th>';
+  echo '<th>Email</th>';
+  echo '<th>Email</th>';
   echo '</tr>';
 
-  foreach ($db->query("SELECT a.id as client_id, a.assigned_therapist_id as assigned_therapist_id, 
-                              a.first_name as first_name, a.last_name as last_name, a.email as email 
-                       FROM account as a 
-                       WHERE a.assigned_therapist_id = '$login_user_id' ") as $row)
+
+  foreach ($db->query("SELECT e.exercise_name, d.discipline_name, m.modality_name, e.assignment, e.video_link,  
+                              ae.assigned_date, ae.active, ae.point_value, 
+                              ae.completed, ae.account_id, ae.exercise_id 
+                              FROM assigned_exercise as ae 
+                              JOIN exercise as e on e.id = ae.exercise_id 
+                              JOIN modality as m on m.id = e.modality_id 
+                              JOIN discipline as d on d.id = e.discipline_id 
+                              WHERE ae.account_id = '$login_user_id' ") as $row)
   {
-    $id = $row['client_id'];
+    $id = $row['account_id'] . $row['exercise_id'];
 
     echo '<tr>';
 
     echo '<td>';      
-    echo '<a href="add_edit_user.php?client_id=' . $id . '">';
-    echo $row['first_name'];      
+    echo '<a href="exercise_detail.php?ae_id=' . $id . '">';
+    echo $row['exercise_name'];      
     echo '</a>';
     echo '</td>';
 
     echo '<td>';
-    echo $row['last_name'];
+    echo $row['discipline_name'];
     echo '</td>';
 
     echo '<td>';
-    echo $row['email'];
+    echo $row['modality_name'];
     echo '</td>'; 
+
+    echo '<td>';
+    echo $row['assignment'];
+    echo '</td>';
+
+    echo '<td>';
+    echo $row['video_link'];
+    echo '</td>';  
+
+    echo '<td>';
+    echo $row['point_value'];
+    echo '</td>';  
+
+    echo '<td>';
+    echo ($row['completed'] == '1' ? 'True' : 'False');
+    echo '</td>';   
+
+    echo '<td>';
+    echo $row['assigned_date'];
+    echo '</td>';  
+
+    echo '<td>';
+    echo ($row['active'] == '1' ? 'True' : 'False');
+    echo '</td>';                          
 
     echo '</tr>';
 
