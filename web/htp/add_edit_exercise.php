a8774534b5037609f66748c875c69e7d931c0b3f
Eric Burns <eburns000@byui.edu>
2019-06-01 21:14:08 -0700

    htp update add edit exercise

--- web/htp/add_edit_exercise.php
+++ web/htp/add_edit_exercise.php
@@ -9,13 +9,22 @@
 
   // get an array of library data
   $statement = $db->query(" SELECT e.id as exercise_id, e.exercise_name as exercise, d.discipline_name as discipline, 
-                                   m.modality_name as modality,  e.assignment as assignment, e.video_link as link, e.active as active
+                                   m.modality_name as modality,  e.assignment as assignment, e.video_link as link, e.active as active,
+                                   e.discipline_id as discipline_id, e.modality_id as modality_id
                             FROM exercise as e
                             JOIN discipline as d on d.id = e.discipline_id
                             JOIN modality as m on m.id = e.modality_id
                             WHERE e.id = '$current_exercise_id' ");
 
   $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+  // get array for modality for drop down select tag
+  $stmtModality = $db->prepare('SELECT id as modality_id, modality_name FROM modality');
+  $stmtModality->execute();  
+
+  // get array for discipline for drop down select tag
+  $stmtDiscipline = $db->prepare('SELECT id as discipline_id, discipline_name FROM discipline');
+  $stmtDiscipline->execute();
 
 ?>
 
@@ -32,52 +41,63 @@
   <h2>Add/Edit Exercise</h2>
   <br>
 
-  <table class="table-standard">
-    <tr>
-      <th>Exercise</th>
-      <th>Discipline</th>
-      <th>Modality</th>
-      <th>Assignment</th>
-      <th>Video</th>
-      <th>Active</th>
-    </tr>
-  
-  <?php 
+  <form action="update_exercise.php" method="post" class="form-checkout">
 
-      echo '<tr>';
+    <input type="hidden" name="id" value="<?php echo $row['exercise_id']; ?>"><br>
 
-      echo '<td>';
-      echo $row['exercise'];
-      echo '</td>';
+    <label for="exercise">Exercise Name</label><br>    
+    <input class="field-checkout" type="text" name="exercise" value="<?php echo $row['exercise']; ?>"><br>
 
-      echo '<td>';
-      echo $row['discipline'];
-      echo '</td>'; 
+    <label for="discipline">Discipline</label>    
+    <select name="discipline_id">
+      <?php
 
-      echo '<td>';
-      echo $row['modality'];
-      echo '</td>'; 
+        // display options for drop down box from clinic table
+        while ($rowDiscipline = $stmtDiscipline->fetch(PDO::FETCH_ASSOC)) {
+          echo "<option value='" . $rowDiscipline['discipline_id'] . "' ";
 
-      echo '<td>';
-      echo $row['assignment'];
-      echo '</td>'; 
+          // set the default selected item based on the assigned clinic
+          if ($rowDiscipline['discipline_id'] == $row['discipline_id']) {
+            echo 'selected';
+          }
 
-      echo '<td>';
-      echo $row['link'];
-      echo '</td>';
+          echo ">" . $rowDiscipline['discipline_name'] . "</option>";
+        } 
 
-      echo '<td>';
-      echo ($row['active'] == '1' ? 'True' : 'False');
-      echo '</td>';
+       ?>
+    </select><br>
 
-      echo '</tr>'; 
+    <label for="modality">Modality</label>    
+    <select name="modality_id">
+      <?php
 
-  ?>
-  
-  </table>
+        // display options for drop down box from clinic table
+        while ($rowModality = $stmtModality->fetch(PDO::FETCH_ASSOC)) {
+          echo "<option value='" . $rowModality['modality_id'] . "' ";
 
+          // set the default selected item based on the assigned clinic
+          if ($rowModality['modality_id'] == $row['modality_id']) {
+            echo 'selected';
+          }
+
+          echo ">" . $rowModality['modality_name'] . "</option>";
+        } 
+
+       ?>
+    </select><br>
+
+    <label for="assignment">Assignment</label><br>
+    <textarea class="field-checkout" name="assignment" rows="10" cols="30"><?php echo $row['assignment']; ?></textarea><br>  
+
+    <label for="link">Video Link</label><br>    
+    <input class="field-checkout" type="text" name="link" value="<?php echo $row['link']; ?>"><br>
+
+    <input class="field-checkout" type="checkbox" name="active" value="1" <?php echo($row['active'] == 1 ? "checked" : ""); ?> > Active?<br>
+
+    <input class="add-button" type="submit" value="Update Exercise"> 
+
+  </form>
   <br>
-
   </div>
   
 </body>
---
+++ web/htp/update_exercise.php
@@ -0,0 +1,81 @@
+<?php
+
+    session_start();
+
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+
+  // Test to see that all fields are in post array and if so, add the record to the database
+  if( isset($_POST['exercise_id']) ) {
+
+    $exercise_id = $_POST['exercise_id'];
+
+    // get current values from table to use as default values
+    $stmtExercise = $db->prepare('SELECT exercise_name, discipline_id, modality_id, assignment, video_link, active
+                                  FROM exercise
+                                  WHERE exercise_id = :exercise_id');
+    $stmtExercise->bindValue(':exercise_id', $exercise_id, PDO::PARAM_INT);
+    $stmtExercise->execute();
+    $rowExercise = $stmtExercise->fetch(PDO::FETCH_ASSOC);
+
+    // sanitize the input and set all variables
+    $exercise_name = ( isset($_POST['exercise_name'])        ? htmlspecialchars($_POST['exercise_name'])    : $rowExercise['exercise_name']);
+    $discipline_id = ( isset($_POST['discipline_id'])        ? htmlspecialchars($_POST['discipline_id'])    : $rowExercise['discipline_id']);
+    $modality_id   = ( isset($_POST['modality_id'])          ? htmlspecialchars($_POST['modality_id'])      : $rowExercise['modality_id']);
+    $assignment    = ( isset($_POST['assignment'])           ? htmlspecialchars($_POST['assignment'])       : $rowExercise['assignment']);
+    $link          = ( isset($_POST['video_link'])           ? htmlspecialchars($_POST['video_link'])       : $rowExercise['video_link']);
+    $active = ( isset($_POST['active']) ? 't' : 'f' );
+
+    // insert values into account table to create another user
+    $stmtUpdate = $db->prepare('UPDATE exercise
+                                SET exercise_name          = :exercise_name,
+                                    discipline_id          = :discipline_id,
+                                    modality_id            = :modality_id,
+                                    assignment             = :assignment,
+                                    video_link             = :video_link,
+                                    active                 = :active
+                                WHERE id = :exercise_id');
+    $stmtUpdate->bindValue(':exercise_name', $exercise_name);
+    $stmtUpdate->bindValue(':discipline_id', $discipline_id);
+    $stmtUpdate->bindValue(':modality_id', $modality_id);
+    $stmtUpdate->bindValue(':assignment', $assignment);
+    $stmtUpdate->bindValue(':video_link', $video_link);
+    $stmtUpdate->bindValue(':active', $active);
+    $stmtUpdate->bindValue(':exercise_id', $exercise_id);
+    $stmtUpdate->execute();
+
+  }
+
+  // go to dashboard page after updating a new user
+  header('Location: library.php');
+  die();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>HTP Update Exercise</title>
+</head>
+<body>
+
+</body>
+</html>
